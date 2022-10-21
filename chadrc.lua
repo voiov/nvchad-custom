a0090f76aded9655eec0ac9240a51ddadcf46004
voiov <silenzaki@gmail.com>
2022-10-21 16:15:42 +0800

    update add plugin alpha-nvim which-key.nvim

--- chadrc.lua
+++ chadrc.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.ui = {
   theme_toggle = { "onedark", "one_light" },
-  theme = "gruvchad",
+  theme = "nord",
 }
 
 M.plugins = require "custom.plugins"
--- plugins/init.lua
+++ plugins/init.lua
@@ -3,7 +3,26 @@ local overrides = require "custom.plugins.overrides"
 return {
 
   -- ["goolord/alpha-nvim"] = { disable = false } -- enables dashboard
+  ["goolord/alpha-nvim"] = {
+    after = "base46",
+    disable = false,
+    config = function()
+      require "plugins.configs.alpha"
+    end,
+  },
 
+  -- Only load whichkey after all the gui
+  ["folke/which-key.nvim"] = {
+    disable = false,
+    module = "which-key",
+    keys = { "<leader>", '"', "'", "`" },
+    config = function()
+      require "plugins.configs.whichkey"
+    end,
+    setup = function()
+      require("core.utils").load_mappings "whichkey"
+    end,
+  },
   -- Override plugin definition options
   ["neovim/nvim-lspconfig"] = {
     config = function()
